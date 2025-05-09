c363640d9415b94990ffaeb72f66bf8f46d7b2e4
McGreger <eric.greger@protonmail.com>
2025-05-09 17:48:53 +0200

    Initial commit

---
+++ module/data/_module.mjs
@@ -0,0 +1,10 @@
+// Export Actors
+export {default as HtbahActorBase} from "./base-actor.mjs";
+export {default as HtbahCharacter} from "./actor-character.mjs";
+export {default as HtbahNPC} from "./actor-npc.mjs";
+
+// Export Items
+export {default as HtbahItemBase} from "./base-item.mjs";
+export {default as HtbahItem} from "./item-item.mjs";
+export {default as HtbahFeature} from "./item-feature.mjs";
+export {default as HtbahSpell} from "./item-spell.mjs";
---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const HTBAH = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+HTBAH.abilities = {
+  str: 'HTBAH.Ability.Str.long',
+  dex: 'HTBAH.Ability.Dex.long',
+  con: 'HTBAH.Ability.Con.long',
+  int: 'HTBAH.Ability.Int.long',
+  wis: 'HTBAH.Ability.Wis.long',
+  cha: 'HTBAH.Ability.Cha.long',
+};
+
+HTBAH.abilityAbbreviations = {
+  str: 'HTBAH.Ability.Str.abbr',
+  dex: 'HTBAH.Ability.Dex.abbr',
+  con: 'HTBAH.Ability.Con.abbr',
+  int: 'HTBAH.Ability.Int.abbr',
+  wis: 'HTBAH.Ability.Wis.abbr',
+  cha: 'HTBAH.Ability.Cha.abbr',
+};
