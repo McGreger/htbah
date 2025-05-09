3d4e1bfb8d16e8aa51fed6db66bbf4a375a71fe4
McGreger <eric.greger@protonmail.com>
2025-05-09 22:37:12 +0200

    fix skill sheets

--- module/dataModels/item-skill.mjs
+++ module/dataModels/item-skill.mjs
@@ -1,3 +1,11 @@
 import HtbahItemBase from "./base-item.mjs";
 
-export default class HtbahSkill extends HtbahItemBase {}
+export default class HtbahSkill extends HtbahItemBase {
+    static defineSchema() {
+        const fields = foundry.data.fields;
+        const schema = {
+            set: new fields.StringField({ initial: "phy"})
+        }
+    return schema;
+    }
+}
