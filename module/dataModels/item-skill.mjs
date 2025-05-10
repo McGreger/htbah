10e32d04956ce34434ec2e7601b62085526dd020
McGreger <eric.greger@protonmail.com>
2025-05-10 02:16:15 +0200

    add skill ranks

--- module/dataModels/item-skill.mjs
+++ module/dataModels/item-skill.mjs
@@ -4,7 +4,8 @@ export default class HtbahSkill extends HtbahItemBase {
     static defineSchema() {
         const fields = foundry.data.fields;
         const schema = {
-            set: new fields.StringField({ initial: "phy"})
+            set: new fields.StringField({ initial: "phy"}),
+            ranks: new fields.NumberField({ initial: 0 })
         }
     return schema;
     }
